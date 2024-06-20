34942a469d49681a65e25b4de9f28569a5d130d8
Kartik Nagrale <115936211+kartnagrale@users.noreply.github.com>
2024-06-20 22:10:19 +0530

    subarray sums 2 cses problemset solution

---
+++ Sorting and Searching/Subarray Sums II.cpp
@@ -0,0 +1,35 @@
+/*
+    Author       :   Kartik Nagrale
+    Codeforces   :   https://codeforces.com/profile/Kartiknagrale
+    Codechef     :   https://www.codechef.com/users/kartiknagrale
+    Leetcode     :   https://leetcode.com/u/kartikknagrale/
+    Github       :   https://github.com/kartnagrale
+    Atcoder      :   https://atcoder.jp/users/Kartiknagrale
+    Connect      :   https://www.linkedin.com/in/kartik-nagrale/
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    ll n,x;cin>>n>>x;
+    unordered_map<ll,ll> m;
+    m[0]=1;
+    ll ans=0,sum=0;
+    for(int i=0;i<n;i++){
+        ll ele;cin>>ele;
+        sum+=ele;
+        if(m.find(sum-x)!=m.end()){
+            ans+=m[sum-x];
+        }
+        m[sum]++;
+    }
+    cout<<ans<<'\n';
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    //ll t;cin>>t;while(t--)
+    solve();
+}
